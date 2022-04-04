cc6b4cb66cf697f7792fc3a554114ef177862b2b
Rick <info@rickdegraaff.nl>
2022-04-04 18:54:47 +0200

    Added config for autoescape

--- src/Config/Twig.php
+++ src/Config/Twig.php
@@ -5,8 +5,10 @@ use CodeIgniter\Config\BaseConfig;
 class Twig extends BaseConfig
 {
     public $functions_safe = [ 'form_hidden', 'json_decode' ];
-    
+
     public $functions_asis = [ 'current_url' ];
 
     public $paths = [];
+
+    public $autoescape = 'html';
 }
