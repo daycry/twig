a2b92844b03999288d15a142ec495af5d288e865
daycry <7590335+daycry@users.noreply.github.com>
2024-05-15 13:34:12 +0200

    Chore

--- src/Config/Twig.php
+++ src/Config/Twig.php
@@ -22,7 +22,7 @@ class Twig extends BaseConfig
     public array $paths = [];
 
     /**
-     * @var array<string, array|string> filters
+     * @var array<string,array|string> filters
      */
     public array $filters = [];
 
