93aefbf888e7ceb77e60bcf791d3e2c251836355
daycry <7590335+daycry@users.noreply.github.com>
2024-05-29 09:28:55 +0200

    Chore

--- src/Common.php
+++ src/Common.php
@@ -1,11 +1,12 @@
 <?php
 
 use Daycry\Twig\Config\Services;
+use Daycry\Twig\Config\Twig as TwigConfig;
 
-if (! function_exists('view')) {
-    function view(string $name, array $data = []): string
+if (! function_exists('twig')) {
+    function twig(string $name, array $data = [], ?TwigConfig $config = null): string
     {
-        $renderer = Services::twig();
+        $renderer = Services::twig($config);
 
         return $renderer->render($name, $data);
     }
